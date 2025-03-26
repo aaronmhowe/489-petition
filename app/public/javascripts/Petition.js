782d0801d01f933f213b70c336dcfc7f48d367ad
aaronmhowe <aaron.howe99@gmail.com>
2025-03-26 08:55:02 -0700

    fixed case-sensitivity bug in event handler where each reference to name, email, city, and state was capitalized in the script but was lowercase in html. added some script and function documentation to event handler and correctly referenced the path to event handler in html.

--- app/public/javascripts/Petition.js
+++ app/public/javascripts/Petition.js
@@ -1,8 +1,17 @@
+/**
+ * Event handler script - listens for interactions from the user and produces a functional response.
+ */
+
 document.addEventListener('DOMContentLoaded', function() {
 
-    const petitionInput = document.getElementById('petition');
+    const petitionForm = document.getElementById('petition');
     const alert = document.getElementById('alert');
     
+    /**
+     * Produces a notification pop-up on the webpage that displays form submission response.
+     * @param {*} message submission success or failure.
+     * @param {*} type alert dismissable.
+     */
     function sendAlert(message, type) {
         const wrapper = document.createElement('div');
         wrapper.innerHTML = `
@@ -15,20 +24,27 @@ document.addEventListener('DOMContentLoaded', function() {
         alert.append(wrapper);
     }
 
+    /**
+     * Defines rules for form signature validation on each input box in the Signature section of the webpage.
+     * @returns pop-up response message from the server - true if conditions are satisfied, false otherwise.
+     */
     function validateForm() {
-        const Name = document.getElementById('Name').value;
-        const Email = document.getElementById('Email').value;
-        const State = document.getElementById('State').value;
+        const Name = document.getElementById('name').value;
+        const Email = document.getElementById('email').value;
+        const State = document.getElementById('state').value;
         let isValid = true;
         let response = [];
+        // enforce name submission within 5 and 20 characters
         if (Name.length < 5 || Name.length > 20) {
             isValid = false;
             response.push('Name should be within 5 to 20 characters in length!');
         }
+        // enforce email include the '@' character
         if (!Email.includes('@')) {
             isValid = false;
             response.push('Email should have an \'@\'!');
         }
+        // enforce input for state be two uppercase letters
         if (State.length !== 2 || State !== State.toUpperCase()) {
             isValid = false;
             response.push('State should be of character length 2 and upper-case!');
@@ -39,15 +55,15 @@ document.addEventListener('DOMContentLoaded', function() {
         };
     }
 
-    petitionInput.addEventListener('submit', function(e) {
+    petitionForm.addEventListener('submit', function(e) {
         e.preventDefault();
         const submissionForm = validateForm();
         if (submissionForm.isValid) {
             const participantData = {
-                Name: document.getElementById('Name').value,
-                Email: document.getElementById('Email').value,
-                City: document.getElementById('City').value,
-                State: document.getElementById('State').value
+                Name: document.getElementById('name').value,
+                Email: document.getElementById('email').value,
+                City: document.getElementById('city').value,
+                State: document.getElementById('state').value
             };
             fetch('/sign', {
                 method: 'POST',
@@ -70,7 +86,7 @@ document.addEventListener('DOMContentLoaded', function() {
                         tbody.innerHTML = '';
                     }
                     tbody.insertBefore(addRow, tbody.firstChild);
-                    petitionInput.reset();
+                    petitionForm.reset();
                     sendAlert(data.message, 'Form Submission Successful.', 'success');
                 } else {
                     sendAlert(data.message, 'Form Submission Failed.', 'danger');
