c2c3df259457cd4b5d5c64756428b87137ba07d7
aaronmhowe <aaron.howe99@gmail.com>
2025-03-26 09:45:41 -0700

    updated submission alert styling to look nice.

--- app/public/javascripts/Petition.js
+++ app/public/javascripts/Petition.js
@@ -17,7 +17,9 @@ document.addEventListener('DOMContentLoaded', function() {
         wrapper.innerHTML = `
             <div class="alert alert-${type} alert-dismissable fade show" role="alert">
                 ${message}
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                <button type="button" class="close" data-bs-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
             </div>
         `;
         alert.innerHTML = '';
