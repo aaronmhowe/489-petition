5255334bfeb4f9f090b5c76aa65ccad6f317bcf3
aaronmhowe <aaron.howe99@gmail.com>
2025-04-21 21:39:56 -0700

    implemented get and add functions for signatures in main app component and renamed component react files

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -3,7 +3,6 @@ import Form from './components/Form';
 import Signatures from './components/Signatures';
 import Alert from './components/Alert';
 import './App.css';
-import { set } from '../../app/app';
 
 /**
  * Main application component.
@@ -82,7 +81,35 @@ function App() {
 
   return (
     <div>
-
+      <div className="navbar">
+        <a href="#">Home</a>
+        <a href="#">Contact</a>
+      </div>
+      <div className="container">
+        <h1 className="header">Move CptS 489 to Afternoon in Winter!</h1>
+        <div className="content">
+          <div className="text">
+            <p>
+              The image you see on the right is a representation of our professor every 
+              morning, right before his much-needed coffee that helps thaw him out a bit. 
+              Imagine having to wake up at 4 or 5 AM in the dead of winter just to prepare 
+              for class. Technically, since the sun hasn’t even risen yet, can we really call 4 
+              AM "morning"? The frigid cold, combined with the mental fog of early hours, 
+              is an unfair battle both for students and faculty alike. No one should have to 
+              endure sub-zero temperatures just to attend an 8 AM lecture. Morning brain 
+              freeze inevitably leads to null pointer exceptions in our heads! For these 
+              reasons, we humbly request the administration to consider shifting CPTS 489 
+              to a more humane afternoon time slot.
+            </p>
+          </div>
+          <div className="image">
+            <img src="/public/images/midterm_image_asset.jpg" alt="Professor" />
+          </div>
+        </div>
+        {alert.show && <Alert message={alert.message} type={alert.type} onClose={() => setAlert({...alert, show: false })} />}
+        <Form onAddSignature={addSignature} />
+        <Signatures signatures={signatures} />
+      </div>
     </div>
   );
 }
