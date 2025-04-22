fa942f96d07f5a05c29ae7dbd928cb6a77557b0d
aaronmhowe <aaron.howe99@gmail.com>
2025-04-21 20:56:54 -0700

    adding new route for form signatures

---
+++ app/routes/signatures.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const Participants = require('../models/Participants');
+
+/**
+ * Retrieve form signatures.
+ * @param {*} req - HTTP Request.
+ * @param {*} res - HTTP Response.
+ */
+router.get('/', async (req, res) => {
+
+});
+
+/**
+ * Insert form signatures.
+ * @param {*} req - HTTP Request.
+ * @param {*} res - HTTP Response.
+ */
+router.insert('/', async (req, res) => {
+
+});
